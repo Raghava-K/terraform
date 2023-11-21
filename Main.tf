92a01f16eb8917f848bfea0f155351ea6b557906
Raghava.K <k.raghavendra227@gmail.com>
2023-11-21 17:54:35 +0530

    new file added

--- Main.tf
+++ Main.tf
@@ -1,10 +1,16 @@
-resource "local_file" "my_cars" {
-    filename = "cars.txt"
-  content = "I Love cars & bikes"
+resource "local_file" "my-devops" {
+filename = var.filename
+content = var.content
+lifecycle {
+create_before_destroy = true
 }
-resource "random_pet" "my_cars" {
-prefix = "mr"
-separator = "."
+}
+
+resource "random_pet" "my-devops" {
+prefix = var.prefix
 length = "1"
-  
+lifecycle {
+
 }
+}
+
